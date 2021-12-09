20b22ddc8dc1c5cdf35ecb5851e5bd7abe293859
thaynarbo <thaynarbo@gmail.com>
2021-12-09 10:11:07 -0300

    cleanup boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import { Contador } from './components/Contador';
+// import { Funcoes } from './typescript/Funcoes';
+// import { ObjetosLiterais } from './typescript/ObjetosLiterais';
+// import { TiposBasicos } from './typescript/TiposBasicos';
+// import { ContadorComHook } from './components/ContadorComHook';
+import { Login } from './components/Login';
+import { Usuarios } from './components/Usuarios';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-2">
+      <h1>Introduccion a TS- React</h1>
+      <hr />
+      {/* <TiposBasicos/> */}
+      {/* <ObjetosLiterais/> */}
+      {/* {<Funcoes/>} */}
+      {/* <Contador/> */}
+      {/* {<ContadorComHook/>} */}
+      {/* <Login/> */}
+      <Usuarios/>
     </div>
-  );
+  )
 }
 
 export default App;
